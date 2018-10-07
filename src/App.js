1dc9b0ff134aa250b0653e3f3c586189cb0291a3
sibyldawn <sibyldawn@gmail.com>
2018-10-07 16:01:17 -0700

    Shallow Rendering

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+constructor(){
+  super();
+  this.state={
+    counter: 0
+  }
+}
+
   render() {
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
+      <div data-test="component-app">
+       <h1 data-test="counter-display"> The counter is currently:</h1>
+        <button data-test="increment-button">Increment counter</button>
       </div>
     );
   }
