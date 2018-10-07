db9f2244b812bdc45a10047b7eba6d94e4587819
sibyldawn <sibyldawn@gmail.com>
2018-10-07 16:13:05 -0700

    Add Button and State Test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-constructor(){
-  super();
+constructor(props){
+  super(props);
+
   this.state={
     counter: 0
   }
@@ -13,8 +14,10 @@ constructor(){
   render() {
     return (
       <div data-test="component-app">
-       <h1 data-test="counter-display"> The counter is currently:</h1>
-        <button data-test="increment-button">Increment counter</button>
+       <h1 data-test="counter-display"> The counter is currently:{this.state.counter}</h1>
+        <button data-test="increment-button" 
+        onClick={() => this.setState({counter: this.state.counter + 1})}
+        >Increment counter</button>
       </div>
     );
   }
