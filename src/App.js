5b30f732b4d57886c8d171ee71e267b0d5b7bdc9
sibyldawn <sibyldawn@gmail.com>
2018-10-07 16:29:41 -0700

    Add Decrement Button

--- src/App.js
+++ src/App.js
@@ -7,17 +7,29 @@ constructor(props){
   super(props);
 
   this.state={
-    counter: 0
+    counter: 0,
+    error:"You can't go lower than zero!",
+    showError: false,
   }
 }
 
   render() {
     return (
       <div data-test="component-app">
-       <h1 data-test="counter-display"> The counter is currently:{this.state.counter}</h1>
+       { this.state.showError ? <h2 data-test="error-display" style={{color:'red'}}>{this.state.error}</h2> : ''}
+       <h1 data-test="counter-display"> The counter is currently:{this.state.counter >= 0 ? this.state.counter : '0'}</h1>
         <button data-test="increment-button" 
         onClick={() => this.setState({counter: this.state.counter + 1})}
         >Increment counter</button>
+        <button data-test="decrement-button" 
+        onClick={() => 
+          {this.state.counter >= 0 
+          ? 
+          this.setState({counter: this.state.counter - 1})
+          :
+          this.setState({showError: true})
+        }}
+        >decrement counter</button>
       </div>
     );
   }
